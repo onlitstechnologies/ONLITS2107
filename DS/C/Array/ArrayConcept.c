240c75131a44586f748edf7e090afed3d8dc2abd
onlitstechnologies <onlitstechnologies@gmail.com>
2022-02-09 12:03:54 +0530

    20220209

---
+++ DS/C/Array/ArrayConcept.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5];
+    printf("The value of arr is %p.\n", arr);
+    printf("The address of arr[0] is %p.\n", &arr[0]);
+    printf("The address of arr[1] is %p.\n", &arr[1]);
+    printf("The address of arr[2] is %p.\n", &arr[2]);
+    printf("The address of arr[3] is %p.\n", &arr[3]);
+    printf("The address of arr[4] is %p.\n", &arr[4]);
+    arr[0] = 10;
+    printf(" arr is pointing to %d.\n", *arr);
+    printf("The value of arr[0] is %d.\n", arr[0]);
+    return 0;
+}
--- PHP/SendEmail.php
+++ PHP/SendEmail.php
@@ -2,6 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 require 'Authenticate.php';
 require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer();    //Instantiating PHPMailer Class
 $mail->isSMTP();
@@ -13,3 +14,19 @@ $mail->Port = 587;
 $mail->Host = "smtp.gmail.com";
 $mail->Username = $username;
 $mail->Password = $password;
+$mail->addAddress("manishmishra.shg@gmail.com");
+$mail->setFrom("$username", "Test PHP Mailer");
+$mail->Subject = "OTP based authentication";
+
+$otp = rand(100000, 999999);
+$content = "The OTP for completion of this process is $otp.";
+
+$mail->msgHTML($content);
+
+if(!$mail->send()) {
+    echo "The was an error while sending email.";
+    var_dump($mail);
+} else {
+    echo "OTP sent successfuly!";
+}
+echo("\n");
