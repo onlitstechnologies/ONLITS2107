6a3724352212f0dca332e7f259288b4e4afe1248
onlitstechnologies <onlitstechnologies@gmail.com>
2022-02-15 09:59:25 +0530

    20220215

--- PHP/SendEmail.php
+++ PHP/SendEmail.php
@@ -1,32 +1,45 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
-require 'Authenticate.php';
-require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
-$mail = new PHPMailer();    //Instantiating PHPMailer Class
-$mail->isSMTP();
-$mail->Mailer = "smtp";
-$mail->SMTPDebug = 1;
-$mail->SMTPAuth = TRUE;
-$mail->SMTPSecure = "tls";
-$mail->Port = 587;
-$mail->Host = "smtp.gmail.com";
-$mail->Username = $username;
-$mail->Password = $password;
-$mail->addAddress("manishmishra.shg@gmail.com");
-$mail->setFrom("$username", "Test PHP Mailer");
-$mail->Subject = "OTP based authentication";
+function send_mail()
+{
+    require 'Authenticate.php';     //Not required
+    require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+    require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
-$otp = rand(100000, 999999);
-$content = "The OTP for completion of this process is $otp.";
+    $mail = new PHPMailer();    //Instantiating PHPMailer Class
+    $mail->isSMTP();
+    $mail->Mailer = "smtp";
+    $mail->SMTPDebug = 1;
+    $mail->SMTPAuth = TRUE;
+    $mail->SMTPSecure = "tls";
+    $mail->Port = 587;
+    $mail->Host = "smtp.gmail.com";
+    $mail->Username = $username;
+    $mail->Password = $password;
+    $mail->addAddress("manishmishra.shg@gmail.com");
+    $mail->setFrom("$username", "Test PHP Mailer");
+    $mail->Subject = "OTP";
 
-$mail->msgHTML($content);
+    $otp = rand(100000, 999999);
+    $content = "The OTP for completion of this process is $otp.";
+    // $content = "Name: $name, Email: $email, Message: $message";
 
-if(!$mail->send()) {
-    echo "The was an error while sending email.";
-    var_dump($mail);
-} else {
-    echo "OTP sent successfuly!";
+    $mail->msgHTML($content);
+
+    if (!$mail->send()) {
+        echo "The was an error while sending email.";
+        var_dump($mail);
+    } else {
+        echo "OTP sent successfuly!";
+    }
+    echo ("\n");
 }
-echo("\n");
+
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $message = $_POST['message'];
+
+// send_mail($name, $email, $message);
+send_mail();
